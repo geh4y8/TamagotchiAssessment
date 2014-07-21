47b337bac7ccb9f3654b4d71968a49add7d1f889
geh4y8 <guy.halperin88@gmail.com>
2014-07-20 18:23:15 -0700

    Finished scripts logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-
-  $("form").submit(function(event){
-    var Purchase = {
-    description1: $("#description").val(),
-    amount: $("#amount").val()
-  }
-    $("#purchases").show();
-    $("#table").append("<tr><td>"+ Purchase.description1 +"</td><td>" + Purchase.amount + "</td></tr>")
-    event.preventDefault();
-  })
-})
+var Tamagotchi = {
+	initialize: function(inputName) {
+		this.name = inputName;
+		this.foodLevel = 10;
+		this.sleepLevel = 10;
+		this.activityLevel = 10;
+	},
+	timePasses: function() {
+		this.foodLevel --;
+	},
+	isAlive: function() {
+		if(this.foodLevel >= 1){
+			return true
+		}else{
+			return false
+		}
+	}
+}
